233c7f8b88e4d556feff23ebf503d80307ab0947
Theresa <thutgreybear@gmail.com>
2023-08-23 15:57:55 +0700

    feat: improve ass 3

--- src/app/component/banner.tsx
+++ src/app/component/banner.tsx
@@ -1,4 +1,33 @@
+"use client";
+import { useState, useEffect } from "react";
 export default function Banner() {
+  const originalText: string =
+    "Vaccines are instrumental in global health, offering immunity against infectious diseases. They stimulate the immune system to recognize and fight pathogens, curbing severe illnesses. Over time, vaccines have eradicated diseases like smallpox and curtailed polio and measles. Despite skepticism, rigorous research and trials ensure vaccine safety. Beyond individual well-being, vaccines establish herd immunity, safeguarding vulnerable groups. Advancements like mRNA technology drive vaccine innovation, fortifying our defenses against emerging diseases. As a symbol of medical progress, vaccines stand as a testament to humanity's commitment to health, embodying our potential to conquer present and future health challenges.";
+
+  const initialTypingDelay: number = 10;
+  const typingSpeed: number = 60;
+
+  const [typedText, setTypedText] = useState<string>("");
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+
+    const startTyping = () => {
+      if (currentIndex <= originalText.length) {
+        setTypedText(originalText.slice(0, currentIndex));
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+        timeout = setTimeout(startTyping, typingSpeed + Math.random() * 20);
+      }
+    };
+
+    timeout = setTimeout(startTyping, initialTypingDelay);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [currentIndex, initialTypingDelay, typingSpeed]);
+
   return (
     <>
       <div
@@ -10,17 +39,14 @@ export default function Banner() {
           backgroundColor: "rgba(255, 255, 255, 0.4)",
         }}
       >
-        Vaccines are instrumental in global health, offering immunity against
-        infectious diseases. They stimulate the immune system to recognize and
-        fight pathogens, curbing severe illnesses. Over time, vaccines have
-        eradicated diseases like smallpox and curtailed polio and measles.
-        Despite skepticism, rigorous research and trials ensure vaccine safety.
-        Beyond individual well-being, vaccines establish herd immunity,
-        safeguarding vulnerable groups. Advancements like mRNA technology drive
-        vaccine innovation, fortifying our defenses against emerging diseases.
-        As a symbol of medical progress, vaccines stand as a testament to
-        humanity's commitment to health, embodying our potential to conquer
-        present and future health challenges.
+        {typedText}
+        <span
+          style={{
+            opacity: typedText.length % 2 === 0 ? 0 : 1,
+          }}
+        >
+          |
+        </span>
       </div>
     </>
   );
--- src/app/component/card.tsx
+++ src/app/component/card.tsx
@@ -1,26 +1,58 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import styles from "../style/card.module.css";
+
+const images = ["/vaccine1.webp", "/vaccine2.webp", "/vaccine3.webp"]; // Add your image URLs here
+
 export default function Card() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000); // Change image every 5 seconds
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <>
-      <div
-        style={{
-          position: "absolute",
-          filter: "brightness(53%) ",
-          backgroundImage: `url('/vaccine.webp')`,
-          zIndex: -1,
-          top: 0,
-        }}
-        className='image'
-      ></div>
+      {images.map((image, index) => (
+        <div
+          key={index}
+          style={{
+            backgroundImage: `url(${image})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            top: 0,
+            zIndex: currentImageIndex === index ? -1 : -10,
+            position: "absolute",
+            filter: "brightness(53%)",
+
+            opacity: currentImageIndex === index ? 1 : 0, // Fade in/out effect
+            transition: "opacity 1s ease-in-out",
+          }}
+          className='image'
+        />
+      ))}
       <div
         style={{
           display: "flex",
+          flexDirection: "column",
           justifyContent: "center",
           fontSize: "50px",
           alignItems: "center",
           height: "50vh",
         }}
       >
-        ประชาสัมพันธ์การให้บริการวัคซีน
+        <h2 style={{ fontSize: "50px", fontWeight: "300" }}>
+          ประชาสัมพันธ์การให้บริการวัคซีน{" "}
+        </h2>
+        <button className={styles.button}>
+          ต้องการความช่วยเหลืออยู่หรือเปล่า -&gt;
+        </button>
       </div>
     </>
   );
